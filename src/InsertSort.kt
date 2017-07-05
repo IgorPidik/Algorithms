04ce619e6b1add933a41f43960ca3e01b4a25413
igor <igorpidik@gmail.com>
2017-07-06 01:54:44 +0200

    clean up

--- src/InsertSort.kt
+++ src/InsertSort.kt
@@ -2,15 +2,15 @@
  * Created by igor on 5.7.2017.
  */
 
-fun insertSort(array: Array<Int>) : Array<Int> {
-    for(i in 1..array.size-1) {
+fun insertSort(array: Array<Int>): Array<Int> {
+    for (i in 1..array.size - 1) {
         val key = array[i]
-        var j = i-1
-        while(j>= 0 && array[j] > key) {
-            array[j+1] = array[j]
+        var j = i - 1
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j]
             j -= 1
         }
-        array[j+1] = key
+        array[j + 1] = key
     }
     return array
 }
--- src/MergeSort.kt
+++ src/MergeSort.kt
@@ -2,47 +2,43 @@
  * Created by igor on 5.7.2017.
  */
 
-fun mergeSort(array: Array<Int>): Array<Int> {
-    return mergeSort(array, 0, array.size-1)
+fun mergeSort(array: Array<Int>) {
+    mergeSort(array, 0, array.size - 1)
 }
 
-private fun mergeSort(array: Array<Int>, startPoint: Int, endPoint: Int): Array<Int> {
-    var arr = array
-    if(startPoint < endPoint) {
-        val midPoint = ((startPoint+endPoint)/2)
-        arr = mergeSort(arr,startPoint, midPoint)
-        arr = mergeSort(arr, midPoint+1, endPoint)
-        return merge(arr,startPoint, midPoint, endPoint)
+private fun mergeSort(array: Array<Int>, startPoint: Int, endPoint: Int) {
+    if (startPoint < endPoint) {
+        val midPoint = ((startPoint + endPoint) / 2)
+        mergeSort(array, startPoint, midPoint)
+        mergeSort(array, midPoint + 1, endPoint)
+        merge(array, startPoint, midPoint, endPoint)
     }
-    return arr
 }
 
 
-private fun merge(array: Array<Int>, startPoint: Int, midPoint: Int, endPoint: Int): Array<Int> {
+private fun merge(array: Array<Int>, startPoint: Int, midPoint: Int, endPoint: Int) {
     val subArray1 = array.sliceArray(startPoint..midPoint)
-    val subArray2 = array.sliceArray(midPoint+1..endPoint)
+    val subArray2 = array.sliceArray(midPoint + 1..endPoint)
 
 
     var firstArrayIndex = 0
     var secondArrayIndex = 0
 
-    for(i in startPoint..endPoint) {
-        if (firstArrayIndex > subArray1.size-1) {
-            for(j in secondArrayIndex..subArray2.size-1) {
-                val posOffset = j-secondArrayIndex
-                array[i+posOffset] = subArray2[j]
+    for (i in startPoint..endPoint) {
+        if (firstArrayIndex > subArray1.size - 1) {
+            for (j in secondArrayIndex..subArray2.size - 1) {
+                val posOffset = j - secondArrayIndex
+                array[i + posOffset] = subArray2[j]
             }
-            return array
-        }
-        else if (secondArrayIndex > subArray2.size-1) {
-            for(j in firstArrayIndex..subArray1.size-1) {
-                val posOffset = j-firstArrayIndex
-                array[i+posOffset] = subArray1[j]
+            return
+        } else if (secondArrayIndex > subArray2.size - 1) {
+            for (j in firstArrayIndex..subArray1.size - 1) {
+                val posOffset = j - firstArrayIndex
+                array[i + posOffset] = subArray1[j]
             }
-            return array
-        }
-        else {
-            if(subArray1[firstArrayIndex] < subArray2[secondArrayIndex]) {
+            return
+        } else {
+            if (subArray1[firstArrayIndex] < subArray2[secondArrayIndex]) {
                 array[i] = subArray1[firstArrayIndex]
                 firstArrayIndex += 1
             } else {
@@ -51,6 +47,4 @@ private fun merge(array: Array<Int>, startPoint: Int, midPoint: Int, endPoint: I
             }
         }
     }
-
-    return array
 }
